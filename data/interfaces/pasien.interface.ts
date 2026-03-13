6e14780818f2466b98e8483be3f52af62741250a
Arif Hakim <arifindo.rahman@gmail.com>
2026-03-13 10:42:21 +0700

    add patient data interface

---
+++ data/interfaces/pasien.interface.ts
@@ -0,0 +1,5 @@
+export interface PasienData {
+  noKK: string;
+  nik: string;
+  nama: string;
+}
---
+++ data/pasien.data.ts
@@ -0,0 +1,7 @@
+import type { PasienData } from './interfaces/pasien.interface';
+
+export const PASIEN_DEFAULT: PasienData = {
+        noKK: '1235324562363676',
+        nik: '3278496512389074',
+        nama: 'Auto Mation',
+};
